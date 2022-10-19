a245e5599118982d84069cf0e9f397e841bdde04
Kevin Jason Williams <72588364+willy22lml@users.noreply.github.com>
2022-10-19 17:20:46 -0300

    Add files via upload

---
+++ nuevocodigo.js
@@ -0,0 +1,10 @@
+console.log("¡Hola mundo! este es mi primer script");
+
+
+document.write("La integración de JavaScript y HTML es muy flexible, ya que existen al menos tres formas para incluir código JavaScript en las páginas web." + "<br>");
+
+document.write("<br>" + "Se pueden incorporar en la cabecera entre las etiquetas HEAD " + "<br>");
+document.write("<br>" + "En el cuerpo de la página entre las etiquetas BODY, a esto se le llama código incrustado." + "<br>");
+document.write("<br>" + "O se puede realizar un archivo externo." + "<br>");
+
+alert('Ver lo simple que es agregar "comillas dobles" ' + " y 'comillas simples'" );
